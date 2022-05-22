999cdabe735e2e8b2f7dc16f709ef9ce235d348d
David Li <1849359095@qq.com>
2022-05-22 20:23:42 +0800

    updated data memory to update through UART

--- new/stage_mem/data_mem.v
+++ new/stage_mem/data_mem.v
@@ -22,43 +22,51 @@ address utilized:
  */
 
 module data_mem #(parameter 
-    ROM_DEPTH = `DEFAULT_ROM_DEPTH              // size of addressable memory
+    ROM_DEPTH = `DEFAULT_ROM_DEPTH                      // size of addressable memory
     )(
     input clk, rst_n,
 
-    input uart_write_enable,                    // uart write enable
-    input [`ISA_WIDTH - 1:0] uart_write_data,   // data from uart
 
-    input [`ISA_WIDTH - 1:0] mem_store_address, // from ex_mem_reg (by alu_result)
-    input [`ISA_WIDTH - 1:0] mem_store_data,    // from ex_mem_reg (by general_reg)
+    input      uart_hazard,                             // from hazard_unit (UART hazard)
+    input      uart_clk,                                // from uart_unit (upg_clk_i)
+    input      uart_write_enable,                       // from uart_unit (upg_wen_i)
+    input      [`ISA_WIDTH - 1:0] uart_data,            // from uart_unit (upg_dat_i)
+    input      [ROM_DEPTH:0] uart_addr,                 // from uart_unit (upg_adr_i)
 
-    input mem_write_enable,                     // from ex_mem_reg (by control_unit)
-    input mem_read_enable,                      // from ex_mem_reg (by control_unit)
-    output [`ISA_WIDTH - 1:0] mem_read_data,    // for mem_wb_reg (the data read form memory)
+    input      [`ISA_WIDTH - 1:0] mem_address,          // from ex_mem_reg (by alu_result)
 
-    input no_op,                                // from ex_mem_reg (by hazard_unit)
+    input      mem_write_enable,                        // from ex_mem_reg (by control_unit)
+    input      [`ISA_WIDTH - 1:0] mem_store_data,       // from ex_mem_reg (by general_reg)
     
-    output keypad_read_enable,                  // signal the keypad to start reading
-    // input [`ISA_WIDTH - 1:0] keypad_read_data,  // data from keypad
+    input      mem_read_enable,                         // from ex_mem_reg (by control_unit)
+    input      [`ISA_WIDTH - 1:0] keypad_read_data,     // data from keypad to output from mem_read_data
+    output     [`ISA_WIDTH - 1:0] mem_read_data,        // for mem_wb_reg (the data read form memory)
 
-    output vga_write_enable,                    // vga write enable
-    output [`ISA_WIDTH - 1:0] vga_store_data    // data to vga
+    input      no_op,                                   // from ex_mem_reg (stop read and write)
+    
+    output     keypad_read_enable,                      // signal the keypad to start reading
+
+    output     vga_write_enable,                        // vga write enable
+    output     [`ISA_WIDTH - 1:0] vga_store_data        // data to vga
     );
 
-    wire io_active = (mem_store_address[`IO_START_BIT:`IO_END_BIT] == `IO_HIGH_ADDR);
+    wire io_active = (mem_address[`IO_START_BIT:`IO_END_BIT] == `IO_HIGH_ADDR);
+    wire uart_instruction_write_enable = uart_write_enable & uart_addr[ROM_DEPTH];
 
-    assign keypad_read_enable = ~mem_store_address[`IO_TYPE_BIT] & io_active & mem_read_enable;
-    assign vga_write_enable   =  mem_store_address[`IO_TYPE_BIT] & io_active & mem_write_enable;
+    assign keypad_read_enable = ~mem_address[`IO_TYPE_BIT] & io_active & mem_read_enable;
+    assign vga_write_enable   =  mem_address[`IO_TYPE_BIT] & io_active & mem_write_enable;
 
     RAM ram(
-        .ena    (~no_op),                               // disabled unpon no_op
+        .ena    (~no_op), // disabled unpon no_op
 
-        .clka   (clk),
-        .addra  (mem_store_address[ROM_DEPTH + 1:2]),   // address unit in bytes
+        .clka   (uart_hazard ? uart_clk                   : clk),
+        .addra  (uart_hazard ? uart_addr[ROM_DEPTH + 1:2] : mem_address[ROM_DEPTH + 1:2]), // address unit in bytes
         .douta  (mem_read_data),
 
-        .wea    (vga_write_enable ? 0 : mem_write_enable),
-        .dina   (vga_write_enable ? 0 : mem_store_data)
+        .dina   (uart_hazard      ? uart_data                     : (
+                 vga_write_enable ? 0                             : mem_store_data)),
+        .wea    (uart_hazard      ? uart_instruction_write_enable : (
+                 vga_write_enable ? 0                             : mem_write_enable))
     );
 
     assign vga_store_data = mem_store_data;
