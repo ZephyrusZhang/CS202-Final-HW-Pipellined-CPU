c775796b98dea571a1157d7b913203412726eb24
David Li <45333770+iDavid2619@users.noreply.github.com>
2022-05-28 16:32:30 +0800

    fixed some bugs

--- new/io_modules/keypad_unit.v
+++ new/io_modules/keypad_unit.v
@@ -12,9 +12,6 @@ module keypad_unit #(parameter
     output reg [7:0] key_coord
     );
     
-    reg [5:0] keypad_state, keypad_next_state;
-    
-    wire key_pressed;
     reg [3:0] col_val, row_val;
     
     localparam SCAN_IDLE     = 8'b0000_0001,
@@ -28,39 +25,41 @@ module keypad_unit #(parameter
     localparam DELAY_TRAN = 2;
     
     reg [20:0] delay_cnt;
-    wire delay_done;
     
-    reg [7:0] pre_state;
-    reg [7:0] next_state;
+    reg [7:0] pre_state, next_state;
     reg [20:0] tran_cnt;
-    wire tran_flag;
     
     always @(negedge clk, negedge rst_n) begin
-        if (!rst_n) delay_cnt <= 'd0;
-        else if (delay_cnt == DEBOUNCE_PERIOD) delay_cnt <= 'd0;
-        else if (next_state == SCAN_JITTER_1 | next_state == SCAN_JITTER_2) begin
-            delay_cnt <= delay_cnt + 1'b1;
-        end else delay_cnt <= 'd0;
+        if (~rst_n) 
+            delay_cnt <= 0;
+        else 
+            case ({delay_cnt == DEBOUNCE_PERIOD, next_state == SCAN_JITTER_1 | next_state == SCAN_JITTER_2})
+                2'b10  : delay_cnt <= 0;
+                2'b01  : delay_cnt <= delay_cnt + 1;
+                default: delay_cnt <= delay_cnt;
+            endcase
     end
     
-    assign delay_done = (delay_cnt == DEBOUNCE_PERIOD - 1'b1) ? 1'b1 : 1'b0;
+    // wire delay_done = (delay_cnt == DEBOUNCE_PERIOD - 1'b1) ? 1'b1 : 1'b0;
     
     always @(negedge clk, negedge rst_n) begin
-        if (!rst_n) begin
-            tran_cnt <= 'd0;
+        if (~rst_n) begin 
+            tran_cnt <= 0;
         end else if (tran_cnt == DELAY_TRAN) begin
-            tran_cnt <= 'd0;
-        end else tran_cnt <= tran_cnt + 1'b1;
+            tran_cnt <= 0;
+        end else 
+            tran_cnt <= tran_cnt + 1;
     end
     
-    // assign tran_flag = (tran_cnt == DELAY_TRAN) ? 1'b1 : 1'b0;
+    // wire tran_flag = (tran_cnt == DELAY_TRAN) ? 1'b1 : 1'b0;
     
     always @(negedge clk, negedge rst_n) begin
-        if (!rst_n) begin
+        if (~rst_n) begin
             pre_state <= SCAN_IDLE;
         end else if (tran_cnt == DELAY_TRAN) begin
             pre_state <= next_state;
-        end else pre_state <= pre_state;
+        end else 
+            pre_state <= pre_state;
     end
     
     always @(*) begin
@@ -68,34 +67,36 @@ module keypad_unit #(parameter
         case (pre_state)
             SCAN_IDLE:
                 if (row_in != 4'hf) next_state = SCAN_JITTER_1;
-                else next_state = SCAN_IDLE;
+                else                next_state = SCAN_IDLE;
             SCAN_JITTER_1:
-                if (row_in != 4'hf && (delay_cnt == DEBOUNCE_PERIOD - 1'b1)) next_state = SCAN_COL1;
-                else next_state = SCAN_JITTER_1;
+                if (row_in != 4'hf & delay_cnt == DEBOUNCE_PERIOD - 1) 
+                                    next_state = SCAN_COL1;
+                else                next_state = SCAN_JITTER_1;
             SCAN_COL1:
                 if (row_in != 4'hf) next_state = SCAN_READ;
-                else next_state = SCAN_COL2;
+                else                next_state = SCAN_COL2;
             SCAN_COL2:
                 if (row_in != 4'hf) next_state = SCAN_READ;
-                else next_state = SCAN_COL3;
+                else                next_state = SCAN_COL3;
             SCAN_COL3:
                 if (row_in != 4'hf) next_state = SCAN_READ;
-                else next_state = SCAN_COL4;
+                else                next_state = SCAN_COL4;
             SCAN_COL4:
                 if (row_in != 4'hf) next_state = SCAN_READ;
-                else next_state = SCAN_IDLE;
+                else                next_state = SCAN_IDLE;
             SCAN_READ:
                 if (row_in != 4'hf) next_state = SCAN_JITTER_2;
-                else next_state = SCAN_IDLE;
+                else                next_state = SCAN_IDLE;
             SCAN_JITTER_2:
-                if (row_in != 4'hf && (delay_cnt == DEBOUNCE_PERIOD - 1'b1)) next_state = SCAN_IDLE;
-                else next_state = SCAN_JITTER_2;
-            default: next_state = SCAN_IDLE;
+                if (row_in != 4'hf & delay_cnt == DEBOUNCE_PERIOD - 1) 
+                                    next_state = SCAN_IDLE;
+                else                next_state = SCAN_JITTER_2;
+            default:                next_state = SCAN_IDLE;
         endcase
     end
     
     always @(negedge clk, negedge rst_n) begin
-        if (!rst_n) begin
+        if (~rst_n) begin
             col_out <= 4'h0;
             row_val <= 4'h0;
             col_val <= 4'h0;
@@ -106,25 +107,25 @@ module keypad_unit #(parameter
                 SCAN_COL3: col_out <= 4'b1101;
                 SCAN_COL4: col_out <= 4'b1110;
                 SCAN_READ: begin
-                    col_out <= col_out;
+                    // col_out <= col_out;
                     row_val <= row_in;
                     col_val <= col_out;
                 end
                 default: col_out <= 4'b0000;
             endcase
         end else begin
-            col_out <= col_out;
+            // col_out <= col_out;
             row_val <= row_val;
             col_val <= col_val;
         end
     end
     
-    assign key_pressed = ((next_state == SCAN_IDLE) && (pre_state == SCAN_JITTER_2) && (tran_cnt == DELAY_TRAN)) ? 1'b1 : 1'b0;
+    wire key_pressed = (next_state == SCAN_IDLE) & (pre_state == SCAN_JITTER_2) & (tran_cnt == DELAY_TRAN);
     
     always @(negedge clk, negedge rst_n) begin
-        if (!rst_n) begin
+        if (~rst_n) begin
             key_coord <= 0; 
-        end else if (key_pressed == 1'b1) begin
+        end else if (key_pressed) begin
             key_coord <= {row_val, col_val};
         end else 
             // key_coord <= key_coord; // critical: annotate when not testing!
