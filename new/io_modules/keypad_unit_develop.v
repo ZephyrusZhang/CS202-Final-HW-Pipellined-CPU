a8072bc582dcfccfcfd2161bf41fbbbee809c635
David Li <45333770+iDavid2619@users.noreply.github.com>
2022-05-29 16:51:03 +0800

    completed the input and keypad unit

--- new/io_modules/keypad_unit_develop.v
+++ new/io_modules/keypad_unit_develop.v
@@ -12,8 +12,6 @@ module keypad_unit_develop #(parameter
     output reg [7:0] key_coord
     );
     
-    reg [7:0] key_coord_1, key_coord_2;
-    
     localparam  SCAN_COL1   = 2'b00,
                 SCAN_COL2   = 2'b01,
                 SCAN_COL3   = 2'b10,
@@ -52,18 +50,16 @@ module keypad_unit_develop #(parameter
                 end
                 2'b10  : begin
                     if (row_in != 4'hf & row_val[stage] == 4'hf) 
-                        key_coord = {col_out, row_in};
+                        key_coord = {row_in, col_out};
                     else 
-                        key_coord = key_coord;
-//                        key_coord = 0;
+                       key_coord = 0;
                     
                     row_val[stage] = row_in;
                     stage = stage + 1;
                     delay_duration = delay_duration + 1;
                 end
                 default: begin
-                    key_coord = key_coord;
-//                    key_coord = 0;
+                    key_coord = 0;
                     delay_duration = delay_duration + 1;
                 end
             endcase
