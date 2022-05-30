0ee79a3b23f83abc446485029e3121acd7b97f66
David Li <45333770+iDavid2619@users.noreply.github.com>
2022-05-30 22:07:08 +0800

    updates to asm assembly file

--- case/unit_test/lw_sw_test/lw_sw/lw_sw_GB.asm
+++ case/unit_test/lw_sw_test/lw_sw/lw_sw_GB.asm
@@ -2,7 +2,9 @@
    buf:   .word  0xFFFFFC60, 0xFFFFFC70	
 .text 0x0000					        
 start:
-	lw $v0,buf($zero)
-	addi $t0,$zero,4
-	sw $v0,buf($t0)
-	j	 start
+	lw 		$v0, buf($zero)
+	lw 		$t1, 0($v0)
+	addi 	$t0, $zero,4
+	sw 		$v0, buf($t0)
+	sw		$t1, 0($v0)
+	j	 	start
