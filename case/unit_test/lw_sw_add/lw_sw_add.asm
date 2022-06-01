0f59adfe1dbf5389d355dc12d84f104b5ec29e81
“Zephyrus” <2996362441@qq.com>
2022-06-01 20:58:33 +0800

    test(assemble): add testcases for each instruction

--- case/unit_test/lw_sw_add/lw_sw_add.asm
+++ case/unit_test/lw_sw_add/lw_sw_add.asm
@@ -2,12 +2,12 @@
    buf:   .word  0xFFFFFC60, 0xFFFFFC70	
 .text 0x0000					        
 start:
-	lw 		$v0, buf($zero)
+	lw 	$v0, buf($zero)
 	lw  	$t1, 0($v0)
 	addi 	$t0, $zero, 4
     
-    add     $t2, $t1, $zero
+	add   $t2, $t1, $zero
 	
-    lw 		$v0, buf($t0)
-	sw		$t2, 0($v0)
-	j	 	start
+	lw 	$v0, buf($t0)
+	sw	$t2, 0($v0)
+	j	 start
