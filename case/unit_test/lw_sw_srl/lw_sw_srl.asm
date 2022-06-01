dda57691be223926b58db8866eb3a97db864ea01
“Zephyrus” <2996362441@qq.com>
2022-06-01 20:28:36 +0800

    update test cases

---
+++ case/unit_test/lw_sw_srl/lw_sw_srl.asm
@@ -0,0 +1,13 @@
+.data  0x0000              		        
+   buf:   .word  0xFFFFFC60, 0xFFFFFC70	
+.text 0x0000					        
+start:
+	lw 		$v0, buf($zero)
+	lw  	$t1, 0($v0)
+	addi 	$t0, $zero, 4
+    
+	srl     $t2, $t1, 1
+	
+	lw 		$v0, buf($t0)
+	sw		$t2, 0($v0)
+	j	 	start
